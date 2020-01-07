e63ad8d271e9223c51497ccf2ce711d3635c66d9
Franco Canedo <francanedo1@hotmail.com>
2020-01-07 20:12:57 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,9 +7,14 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-        :name => "benovoilo",
+        :name => "Benovoilo",
         :age => "17",
         :attitude => "worried"
+        },
+        {
+        :name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"
         }
       ]
    }, 
@@ -19,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+         {
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
+        },
+        {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+        }
       ]
    }
 }
